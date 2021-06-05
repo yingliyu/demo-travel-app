9601297c50c6f3f2f309b8b9fb9d381e910e2692
yingliyu <404997046@qq.com>
2021-06-05 22:30:47 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./app.vue";
 import router from "./router";
 import store from "./store";
+import fastClick from "fastclick";
+import "./styles/reset.css";
+import "./styles/border.css"; // 移动端1px边框的问题
 
 Vue.config.productionTip = false;
+//某些机型某些浏览器300ms点击延迟
+fastClick.attach(document.body);
 
 new Vue({
   router,
